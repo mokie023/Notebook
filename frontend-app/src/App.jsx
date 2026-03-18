1d8156538121234a98b22873459a3305498c12bc
mokie023 <makwaibamoeketsikaho@gmail.com>
2026-03-18 19:44:00 +0200

    Adding dashboard routes

--- frontend-app/src/App.jsx
+++ frontend-app/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Callback from "./pages/Callback";
+import Dashboard from "./pages/Dashboard";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/auth/callback" element={<Callback />} />
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
 }
